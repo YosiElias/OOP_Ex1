680833d53a760bc193086bf5190b076edb418316
YosiElias <73485442+YosiElias@users.noreply.github.com>
2021-11-18 01:03:12 +0200

    Update ReadFromJson.py

--- .idea/ReadFromJson.py
+++ .idea/ReadFromJson.py
@@ -4,34 +4,24 @@ from Elevator import Elevator
 
 def read(filePath:str) -> Building:
     # Opening JSON file
-    # f = open('../venv/B3.json')
     f = open(str(filePath))
 
-    # returns JSON object as
-    # a dictionary
+    # returns JSON object as a dictionary
     data = json.load(f)
 
     # Iterating through the json
     minFloor = data['_minFloor']
     maxFloor = data['_maxFloor']
     elevList = data['_elevators']
-    # print(maxFloor)
     b = Building(_minFloor=minFloor, _maxFloor=maxFloor)
 
-    for elv in elevList:
+    for elv in elevList:    # build all elevators
         e = Elevator(id=elv['_id'], speed=elv['_speed'], minFloor=elv['_minFloor'], maxFloor=elv['_maxFloor'],
                      closeTime=elv['_closeTime'], openTime=elv['_openTime'],  startTime=elv['_startTime'],
                      stopTime=elv['_stopTime'])
-        b+=e
-        # print(b.get_elevDict()[str(e.get_id())])
+        b+=e    # We override the 'add' operation to get more comfortable and clean code
 
-    # print(b) #Todo: print b, for testing. dbs
-    # Closing file
     f.close()
-    return b
+    return b  # Building
 
 
-
-if __name__ == '__main__':
-    b = read('../venv/B3.json')
-    print(b.get_minFloor())
